1f2c98cc23143f58d53c9329f0168b37a2f950b2
Maral <mrlmosafercomputer@gmail.com>
2024-05-14 23:30:45 +0330

    Add Data

--- MongoDb_Project/Program.cs
+++ MongoDb_Project/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDb_Project.Entities;
+using MongoDb_Project.Repositories;
+
+ProductRepository product = new();
+
+product.Add(new Product
+{
+    Category = "لپ تاپ",
+    Description = "این لپ تاپ خیلی خفنه بچه ها پس باید گیتموفالو کنید:|",
+    Name = "ایسوس فلان فلان",
+    Price = 40000000,
+});
+
+Console.ReadKey();
