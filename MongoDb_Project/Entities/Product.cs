f03b535d1dab922300625cdd7bc301da2ab40869
Maral <mrlmosafercomputer@gmail.com>
2024-05-14 22:56:30 +0330

    Model: Product

---
+++ MongoDb_Project/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDb_Project.Entities
+{
+    public class Product
+    {
+        [BsonId] //مپ میشه به آیدی مونگو
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public int Price { get; set; }
+    }
+}
