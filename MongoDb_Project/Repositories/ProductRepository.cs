3869b255dd6a7218663ad8d02128d2ed1bac97ad
Maral <mrlmosafercomputer@gmail.com>
2024-05-14 23:02:35 +0330

    ProductRepository(Insert)

---
+++ MongoDb_Project/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using MongoDb_Project.Entities;
+
+namespace MongoDb_Project.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly IMongoDatabase _db;
+        //تایپ داکیومنت توی برنامه چیه
+        private readonly IMongoCollection<Product> _productCollection;
+
+        public ProductRepository()
+        {
+            //میتونه متصل شه به دیتابیس
+            var client = new MongoClient();
+
+            //دیتابیس
+            _db = client.GetDatabase("MongoStore");
+
+            //کالکشن-جداول
+            _productCollection = _db.GetCollection<Product>("Product");
+        }
+
+        #region Insert
+        public void Add(Product product)
+        {
+            _productCollection.InsertOne(product);
+        }
+        #endregion
+    }
+}
