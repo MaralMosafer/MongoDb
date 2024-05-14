d8388efaec281fb745bc931ca87b11fea57e742f
Maral <mrlmosafercomputer@gmail.com>
2024-05-14 23:51:52 +0330

    Sample Data

--- MongoDb_Project/Program.cs
+++ MongoDb_Project/Program.cs
@@ -1,20 +1,43 @@
 ﻿using MongoDb_Project.Entities;
 using MongoDb_Project.Repositories;
 
-ProductRepository product = new();
+ProductRepository productRepository = new();
+//اینجا ایدی خودتون رو وارد کنید
+var product = productRepository.FinddBy(Guid.Parse("Enter Your guid!"));
 
-product.Add(new Product
+#region Add
+productRepository.Add(new Product
 {
     Category = "لپ تاپ",
     Description = "این لپ تاپ خیلی خفنه بچه ها پس باید گیتموفالو کنید:|",
     Name = "ایسوس فلان فلان",
     Price = 40000000,
 });
+#endregion
 
-var getProducts = product.GetList();
+#region List
+var getProducts = productRepository.GetList();
 foreach (var prd in getProducts)
 {
     Console.WriteLine($"Id:{prd.Id} Name:{prd.Name} ");
 }
+#endregion
+
+#region Edit
+if (product is not null)
+{
+    Console.WriteLine("Founded");
+    product.Name = "نام جدید";
+    product.Price = 10;
+    product.Description = "توضیحات جدید";
+    product.Category = "دسته بندی جدید";
+
+    productRepository.Edit(product.Id, product);
+}
+#endregion
+
+#region Delete
+productRepository.Delete(product.Id);
+#endregion
 
 Console.ReadKey();
